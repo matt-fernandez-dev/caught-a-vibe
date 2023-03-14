acad72a175bbf8bd7fae6e9dad0d7dbcf1812507
matt-fernandez-dev <matt.fernandez.dev@gmail.com>
2023-03-13 22:48:56 -0500

    Connected Firebase To Application

---
+++ caught-a-vibe/App/caught_a_vibeApp.swift
@@ -0,0 +1,31 @@
+//
+//  caught_a_vibeApp.swift
+//  caught-a-vibe
+//
+//  Created by Matthew Fernandez on 3/13/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct caught_a_vibeApp: App {
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
