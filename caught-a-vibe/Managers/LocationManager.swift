27c874dd638d4153936587139ef331d5ee2c060a
matt-fernandez-dev <matt.fernandez.dev@gmail.com>
2023-03-14 00:35:40 -0500

    Setup Location Manager

---
+++ caught-a-vibe/Managers/LocationManager.swift
@@ -0,0 +1,27 @@
+//
+//  LocationManager.swift
+//  caught-a-vibe
+//
+//  Created by Matthew Fernandez on 3/14/23.
+//
+
+import CoreLocation
+
+class LocationManger: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManger: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
