eb606b74449f4476be2f24f8d2e074224fcc58e2
matt-fernandez-dev <matt.fernandez.dev@gmail.com>
2023-03-13 22:11:02 -0500

    Initial Commit

---
+++ caught-a-vibe/caught_a_vibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  caught_a_vibeApp.swift
+//  caught-a-vibe
+//
+//  Created by Matthew Fernandez on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct caught_a_vibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
